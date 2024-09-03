d66ca8affa168f94100e4d665421278d22c521ad
AbhineetPuranik <abhineetpuranik@gmail.com>
2024-09-04 00:11:00 +0530

    Add

---
+++ Q7.java
@@ -0,0 +1,18 @@
+public class Q7 {
+    public static void main(String[] args) {
+        int grades=93;
+        if(grades>=90){
+            System.out.println("Grade A");
+        }else if(grades>=80){
+            System.out.println("Grade B");
+        }else if(grades>=70){
+            System.out.println("Grade C");
+        }else if(grades>=60){
+            System.out.println("Grade D");
+        }else if(grades>=40){
+            System.out.println("Grade E");
+        }else{
+            System.out.println("Grade F");
+        }
+    }
+}
---
+++ Q8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Dividend: ");
+        double dn=sc.nextDouble();
+        System.out.println("Enter Divisor: ");
+        double dv=sc.nextDouble();
+        if(dn%dv==0){
+            System.out.println(dn+" is divisible by "+dv);
+        }else{
+            System.out.println("Not divisible");
+        }
+    }
+}
